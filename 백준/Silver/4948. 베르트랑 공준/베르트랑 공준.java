87ef94e8fdff383043d62d0b280a7489a002974d
taehoon <109726278+pth2134@users.noreply.github.com>
2023-06-10 12:12:40 +0900

    [Silver II] Title: 베르트랑 공준, Time: 192 ms, Memory: 15840 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -7,26 +7,35 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
+		ArrayList<Integer> max = new ArrayList<>();
+		ArrayList<Integer> order = new ArrayList<>();
 		while(true) {
-			int n = Integer.parseInt(br.readLine());
-			int m = 2*n;
-			if(n==0) break;
-			boolean[] prime = new boolean[m+1];
-			prime[0] = true;
-			prime[1] = true;
-			for(int i =2; i<=Math.sqrt(m); i++) {
-				if(!prime[i]) {
-					for(int j =i*i;j<=m; j+=i) {
-						prime[j] =true;
-					}
+			int value = Integer.parseInt(br.readLine());
+			if(value ==0) break;
+			max.add(value);
+			order.add(value);
+		}
+		
+		Collections.sort(max);
+		int n = max.get(max.size()-1);
+		int m = 2*n;
+		boolean[] prime = new boolean[m+1];
+		prime[0]=prime[1]=true;
+		
+		for(int i =2; i<=Math.sqrt(m); i++) {
+			if(!prime[i]) {
+				for(int j =i*i; j<=m; j+=i) {
+					prime[j] = true;
 				}
 			}
-			
-			int cnt =0;
-			for(int i =n+1; i<=m; i++) {
-				if(!prime[i]) {
-					cnt++;
-				}
+		}
+		
+		for(int i =0; i<order.size(); i++) {
+			int cnt = 0;
+			int n2 = order.get(i);
+			int m2 = 2*n2;
+			for(int j=n2+1; j<=m2; j++) {
+				if(!prime[j]) cnt++;
 			}
 			bw.write(cnt+"\n");
 		}
