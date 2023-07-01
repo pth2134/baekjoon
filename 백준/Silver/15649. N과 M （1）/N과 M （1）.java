ff0eaa8679ace398d5fd0ca245a1b72199497f5e
taehoon <109726278+pth2134@users.noreply.github.com>
2023-07-01 10:33:31 +0900

    [Silver III] Title: N과 M (1), Time: 280 ms, Memory: 22156 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -2,39 +2,38 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static StringBuilder str = new StringBuilder();
+	public static int n;
+	public static int m;
+	public static StringBuilder str = new StringBuilder();
+	public static boolean[] check;
+	public static int[] result;
 	
-	
-	public static void rise(int n, int m, boolean[] check, int[] each) {
-		if(m==0) {
-			//출력(거꾸로 해야함) m부터 1까지 담고 0에서는 출력만함
-			for(int i =each.length-1; i>=1; i--) {
-				str.append(each[i]).append(" ");
+	public static void sequence(int index) {
+		if(index == m) {
+			for(int i=0; i<m; i++) {
+				str.append(result[i]).append(" ");
 			}
 			str.append("\n");
 		} else {
-			//1부터 현재 있는 곳을 true로 설정 후 다음 자리 탐색
-			for(int i =1; i<=n; i++) {
+			for(int i=1; i<=n; i++) {
 				if(!check[i]) {
 					check[i] = true;
-					each[m] = i;
-					rise(n,m-1,check,each);
-					check[i] =false;
+					result[index] = i;
+					sequence(index+1);
+					check[i] = false;
 				}
 			}
 		}
 	}
 	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		boolean[] check = new boolean[n+1];
-		int[] each = new int[m+1];
-		rise(n,m,check,each);
-		System.out.println(str);
-	}
-	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        check = new boolean[n+1];
+        result = new int[m+1];
+        sequence(0);
+        System.out.println(str);
+    }
 }
