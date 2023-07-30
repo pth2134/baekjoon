fac4ccd6c83081ade3c904a080c4e17cc119288a
taehoon <109726278+pth2134@users.noreply.github.com>
2023-07-30 19:29:04 +0900

    [Silver I] Title: 회의실 배정, Time: 512 ms, Memory: 42296 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -19,7 +19,7 @@ class Main{
 			}
 			
 			Arrays.sort(meeting, (o1, o2) -> {
-				if(o1[1]==o2[1]) { return o1[0]-o2[1];}
+				if(o1[1]==o2[1]) { return o1[0]-o2[0];}
 				else return o1[1]-o2[1];}
 			);
 			
