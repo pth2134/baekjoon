168da13ffa982232d7863a92a681a12c6e07c3f4
taehoon <109726278+pth2134@users.noreply.github.com>
2023-08-01 00:06:12 +0900

    [Silver III] Title: 주유소, Time: 336 ms, Memory: 30112 KB -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -11,31 +11,22 @@ class Main{
 			int n = Integer.parseInt(br.readLine());
 			
 			int[] distance = new int[n]; //distance[i] = i번쨰 인덱스 도시까지의 거리
-			int[][] price = new int[n][2];
 			
 			st= new StringTokenizer(br.readLine());
 			for(int i =1; i<n; i++) {
-				distance[i] = distance[i-1]+Integer.parseInt(st.nextToken());
+				distance[i] = Integer.parseInt(st.nextToken());
 			}
+			
 			st= new StringTokenizer(br.readLine());
-			for(int i =0; i<n; i++) {
-				price[i][0] = i;
-				price[i][1] = Integer.parseInt(st.nextToken());
+			int min = Integer.parseInt(st.nextToken());
+			long cost =  min*distance[1];;
+			int price = 0;
+			for(int i =1; i<n-1; i++) {
+				price = Integer.parseInt(st.nextToken());
+				min = min > price ? price : min;
+				cost += min*distance[i+1];
 			}
 			
-			Arrays.sort(price, (o1,o2)->{
-				if(o1[1]==o2[1]) return o1[0]-o2[0];
-				return o1[1]-o2[1];
-			});
-			
-			long cost = (long) price[0][1]*(distance[n-1]-distance[price[0][0]]);
-			int city = price[0][0];
-			for(int i =1; i<n; i++) {
-				if(price[i][0]<city) {
-					cost+= (long) price[i][1]*(distance[city]-distance[price[i][0]]);
-					city = price[i][0];
-				}
-			}
 			bw.write(cost+"");
 			bw.flush();
 		}
