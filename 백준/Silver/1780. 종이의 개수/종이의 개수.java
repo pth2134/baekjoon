a4cab4c618ef9351a60ed09a6d8e337453bac82e
taehoon <109726278+pth2134@users.noreply.github.com>
2023-08-10 23:40:25 +0900

    [Silver II] Title: 종이의 개수, Time: 916 ms, Memory: 310696 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.java
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.java
@@ -6,26 +6,17 @@ class Main{
 	static StringBuilder str = new StringBuilder();
 	static int[] mzp = new int[3]; //0 m, 1 z, 2 p
 	
-	static void check(int x, int y, int n, int size) {
+	static void check(int x, int y, int n) {
 		int begin = paper[y][x];
 		for(int i = y; i< y+n; i++) {
 			for(int j = x; j<x+n; j++) {
 				if(paper[i][j]!=begin) {
 					int next = n/3;
-					int nextS = size/9;
-					int x1 = x+next;
-					int y1 = y+next;
-					int x2 = x1+next;
-					int y2 = y1+next;
-					check(x,y,next,nextS);
-					check(x1,y,next,nextS);
-					check(x2,y,next,nextS);
-					check(x,y1,next,nextS);
-					check(x1,y1,next,nextS);
-					check(x2,y1,next,nextS);
-					check(x,y2,next,nextS);
-					check(x1,y2,next,nextS);
-					check(x2,y2,next,nextS);
+					for(int k =0; k <3; k++) {
+						for(int l =0; l<3; l++) {
+							check(x+l*next,y+k*next,next);
+						}
+					}
 					return;
 				}
 			}
@@ -46,7 +37,7 @@ class Main{
 				}
 			}
 
-			check(1,1,n,n*n);
+			check(1,1,n);
 			str.append(mzp[0]).append("\n").append(mzp[1]).append("\n").append(mzp[2]);
 			System.out.println(str);
 		}
