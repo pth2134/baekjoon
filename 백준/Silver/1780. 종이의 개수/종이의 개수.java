f7bf52c46be252f47601c0e5f25afa6bae429e17
taehoon <109726278+pth2134@users.noreply.github.com>
2023-08-10 23:34:13 +0900

    [Silver II] Title: 종이의 개수, Time: 1020 ms, Memory: 312564 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.java
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.java
@@ -2,66 +2,52 @@ import java.io.*;
 import java.util.*;
 
 class Main{
-	static int[][] screen;
-	static int[][][] dp;
-	static boolean clear = false;
+	static int[][] paper;
 	static StringBuilder str = new StringBuilder();
-	static int mCnt = 0;
-	static int zCnt = 0;
-	static int pCnt = 0;
+	static int[] mzp = new int[3]; //0 m, 1 z, 2 p
 	
 	static void check(int x, int y, int n, int size) {
-		
-		int s1 = dp[y+n][x+n][0]-dp[y+n][x][0]-dp[y][x+n][0]+dp[y][x][0];
-		int s2 = dp[y+n][x+n][1]-dp[y+n][x][1]-dp[y][x+n][1]+dp[y][x][1];
-		if(s1==size) {
-			pCnt++;
-		} else if(s2==size) {
-			mCnt++;
-		} else if(s1==0&&s2==0) {
-			zCnt++;
-		} else {
-			int next = n/3;
-			int nextS = size/9;
-			int x1 = x+next;
-			int y1 = y+next;
-			int x2 = x1+next;
-			int y2 = y1+next;
-			check(x,y,next,nextS);
-			check(x1,y,next,nextS);
-			check(x2,y,next,nextS);
-			check(x,y1,next,nextS);
-			check(x1,y1,next,nextS);
-			check(x2,y1,next,nextS);
-			check(x,y2,next,nextS);
-			check(x1,y2,next,nextS);
-			check(x2,y2,next,nextS);
+		int begin = paper[y][x];
+		for(int i = y; i< y+n; i++) {
+			for(int j = x; j<x+n; j++) {
+				if(paper[i][j]!=begin) {
+					int next = n/3;
+					int nextS = size/9;
+					int x1 = x+next;
+					int y1 = y+next;
+					int x2 = x1+next;
+					int y2 = y1+next;
+					check(x,y,next,nextS);
+					check(x1,y,next,nextS);
+					check(x2,y,next,nextS);
+					check(x,y1,next,nextS);
+					check(x1,y1,next,nextS);
+					check(x2,y1,next,nextS);
+					check(x,y2,next,nextS);
+					check(x1,y2,next,nextS);
+					check(x2,y2,next,nextS);
+					return;
+				}
+			}
 		}
+		
+		mzp[begin+1]++;
 	}
 	
 	public static void main(String[] args) throws IOException {
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			StringTokenizer st;
 			int n = Integer.parseInt(br.readLine());
-			
-			dp = new int[n+1][n+1][2];
-			screen = new int[n+1][n+1];
+			paper = new int[n+1][n+1];
 			for(int i =1; i<=n; i++) {
 				st = new StringTokenizer(br.readLine());
 				for (int j = 1; j <=n; j++) {
-					screen[i][j] = Integer.parseInt(st.nextToken());
-					if(screen[i][j]>=0) {
-						dp[i][j][0] = dp[i-1][j][0]+dp[i][j-1][0]-dp[i-1][j-1][0]+screen[i][j];
-						dp[i][j][1] = dp[i-1][j][1]+dp[i][j-1][1]-dp[i-1][j-1][1];
-					} else {
-						dp[i][j][0] = dp[i-1][j][0]+dp[i][j-1][0]-dp[i-1][j-1][0];
-						dp[i][j][1] = dp[i-1][j][1]+dp[i][j-1][1]-dp[i-1][j-1][1]+1;
-					}
+					paper[i][j] = Integer.parseInt(st.nextToken());	
 				}
 			}
 
-			check(0,0,n,n*n);
-			str.append(mCnt).append("\n").append(zCnt).append("\n").append(pCnt);
+			check(1,1,n,n*n);
+			str.append(mzp[0]).append("\n").append(mzp[1]).append("\n").append(mzp[2]);
 			System.out.println(str);
 		}
  }
