bd16d5f7bb7ee67e71c56dd1d95bdfaf9c48cf10
taehoon <109726278+pth2134@users.noreply.github.com>
2024-01-03 08:26:29 +0900

    [Bronze V] Title: 웰컴, Time: 116 ms, Memory: 13888 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append(".  .   .\n").append("|  | _ | _. _ ._ _  _\n").append("|/\\|(/.|(_.(_)[ | )(/.");
+        System.out.println(sb);
+    }
+
+}
