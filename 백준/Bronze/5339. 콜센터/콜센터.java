6d667088c72c0169c166d23dab1701c059b3b3ba
taehoon <109726278+pth2134@users.noreply.github.com>
2024-01-07 14:48:30 +0900

    [Bronze V] Title: 콜센터, Time: 124 ms, Memory: 14040 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("     /~\\\n")
+                .append("    ( oo|\n")
+                .append("    _\\=/_\n")
+                .append("   /  _  \\\n")
+                .append("  //|/.\\|\\\\\n")
+                .append(" ||  \\ /  ||\n")
+                .append("============\n")
+                .append("|          |\n")
+                .append("|          |\n")
+                .append("|          |\n");
+        System.out.println(sb);
+    }
+
+}
