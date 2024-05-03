1807992a2f796fef6fc3cc24948f7623334ad77c
taehoon <xogns2134@gmail.com>
2024-05-03 13:49:51 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1952 ms, Memory: 365304 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -1,13 +1,10 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+	static int[] union;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,48 +12,52 @@ public class Main {
 		StringBuilder sb = new StringBuilder();
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
-		int[] visit = new int[n + 1];
+		union = new int[n + 1];
 
-		List<Queue<Integer[]>> list = new ArrayList<>();
-		for (int i = 0; i <= n; i++) {
-			list.add(new LinkedList<>());
+		for (int i = 1; i <= n; i++) {
+			union[i] = i;
 		}
+
+		PriorityQueue<Integer[]> pq = new PriorityQueue<>((o1, o2) -> o1[2] - o2[2]);
 
 		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
 			int cost = Integer.parseInt(st.nextToken());
-			list.get(from).add(new Integer[] {to, cost});
-			list.get(to).add(new Integer[] {from, cost});
+
+			pq.add(new Integer[] {from, to, cost});
 		}
 
-		PriorityQueue<Integer[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
-		pq.add(new Integer[] {1, 1});
-
-		while (!pq.isEmpty()) {
-			Integer[] poll = pq.poll();
-			if (visit[poll[0]] != 0)
-				continue;
-			visit[poll[0]] = poll[1];
-			Queue<Integer[]> que = list.get(poll[0]);
-			while (!que.isEmpty()) {
-				Integer[] q = que.poll();
-				if (visit[q[0]] == 0) {
-					pq.add(new Integer[] {q[0], q[1]});
-				}
-			}
-		}
-
+		int cnt = 0;
 		int sum = 0;
 		int max = 0;
-		for (int i = 2; i <= n; i++) {
-			sum += visit[i];
-			max = Math.max(max, visit[i]);
+
+		while (cnt < n - 1) {
+			Integer[] poll = pq.poll();
+			int a = unionFind(poll[0]);
+			int b = unionFind(poll[1]);
+
+			if (a == b)
+				continue;
+
+			if (b > a)
+				union[b] = a;
+			else
+				union[a] = b;
+			cnt++;
+			sum += poll[2];
+			max = Math.max(max, poll[2]);
 		}
 
 		System.out.println(sum - max);
 
+	}
+
+	public static int unionFind(int num) {
+		if (union[num] != num)
+			return unionFind(union[num]);
+		return num;
 	}
 
 }
