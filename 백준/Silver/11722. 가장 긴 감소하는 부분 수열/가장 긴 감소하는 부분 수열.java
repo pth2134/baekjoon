1406d8cd9773b5b3204b6c340f481644dcbee140
taehoon <xogns2134@gmail.com>
2024-06-23 15:39:34 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 124 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -0,0 +1,82 @@
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Reader reader = new Reader();
+		int n = reader.nextInt();
+		int[] arr = new int[n];
+		int last = 0;
+
+		for (int i = 0; i < n; i++) {
+			int input = reader.nextInt();
+
+			if (input > arr[last]) {
+				int lo = 0;
+				int hi = last;
+				int mid;
+				while (lo < hi) {
+					mid = (lo + hi) / 2;
+					if (arr[mid] > input) {
+						lo = mid + 1;
+					} else {
+						hi = mid;
+					}
+				}
+
+				arr[lo] = input;
+
+			} else if (input < arr[last]) {
+				last++;
+				arr[last] = input;
+			}
+		}
+
+		System.out.println(last + 1);
+	}
+
+	static class Reader {
+		final int SIZE = 1 << 15;
+		byte[] buffer = new byte[SIZE];
+		int index, size;
+
+		int nextInt() throws IOException {
+			int n = 0;
+			byte c;
+			while ((c = read()) <= 32)
+				;
+			boolean neg = c == '-';
+			if (neg)
+				c = read();
+			do
+				n = (n << 3) + (n << 1) + (c & 15); while (isNumber(c = read()));
+			return neg ? -n : n;
+		}
+
+		long nextLong() throws IOException {
+			long n = 0;
+			byte c;
+			while ((c = read()) <= 32)
+				;
+			boolean neg = c == '-' ? true : false;
+			if (neg)
+				c = read();
+			do
+				n = (n << 3) + (n << 1) + (c & 15); while (isNumber(c = read()));
+			return neg ? -n : n;
+		}
+
+		boolean isNumber(byte c) {
+			return 47 < c && c < 58;
+		}
+
+		byte read() throws IOException {
+			if (index == size) {
+				size = System.in.read(buffer, index = 0, SIZE);
+				if (size < 0)
+					buffer[0] = -1;
+			}
+			return buffer[index++];
+		}
+	}
+}
