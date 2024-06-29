95df0c27fce6d15ee03a3ca394fb71eb45462f12
taehoon <xogns2134@gmail.com>
2024-06-29 16:32:23 +0900

    [Gold V] Title: 돌 게임 6, Time: 104 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Gold/9660. 돌 게임 6/돌 게임 6.java
@@ -0,0 +1,50 @@
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Reader reader = new Reader();
+		long n = reader.nextLong();
+		boolean[] skWin_until_6 = {false, true, false, true, true, true, true};
+		boolean[] skWin = {false, true, false, true, true, true, true};
+
+		if (n <= 6) {
+			System.out.println(skWin_until_6[(int)n] ? "SK" : "CY");
+		} else {
+			System.out.println(skWin[(int) (n % 7)] ? "SK" : "CY");
+		}
+
+	}
+
+	static class Reader {
+		final int SIZE = 1 << 15;
+		byte[] buffer = new byte[SIZE];
+		int index, size;
+
+		long nextLong() throws IOException {
+			long n = 0;
+			byte c;
+			while ((c = read()) <= 32)
+				;
+			boolean neg = c == '-' ? true : false;
+			if (neg)
+				c = read();
+			do
+				n = (n << 3) + (n << 1) + (c & 15); while (isNumber(c = read()));
+			return neg ? -n : n;
+		}
+
+		boolean isNumber(byte c) {
+			return 47 < c && c < 58;
+		}
+
+		byte read() throws IOException {
+			if (index == size) {
+				size = System.in.read(buffer, index = 0, SIZE);
+				if (size < 0)
+					buffer[0] = -1;
+			}
+			return buffer[index++];
+		}
+	}
+}
