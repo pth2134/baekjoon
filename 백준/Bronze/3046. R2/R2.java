9ff9dc0105bee3f31a6b0d62e7eccda36d05e2b7
taehoon <xogns2134@gmail.com>
2024-07-16 15:53:06 +0900

    [Bronze IV] Title: R2, Time: 108 ms, Memory: 14028 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		Reader reader = new Reader();
+		int r1 = reader.nextInt();
+		int s = reader.nextInt();
+
+		System.out.println(s*2-r1);
+	}
+
+	static class Reader {
+		final int SIZE = 1 << 15;
+		byte[] buffer = new byte[SIZE];
+		int index, size;
+
+		int nextInt() throws IOException {
+			int n = 0;
+			byte c;
+			while ((c = read()) <= 32)
+				;
+			boolean neg = c == '-';
+			if (neg)
+				c = read();
+			do
+				n = (n << 3) + (n << 1) + (c & 15); while (isNumber(c = read()));
+			return neg ? -n : n;
+		}
+
+
+		boolean isNumber(byte c) {
+			return 47 < c && c < 58;
+		}
+
+		byte read() throws IOException {
+			if (index == size) {
+				size = System.in.read(buffer, index = 0, SIZE);
+				if (size < 0)
+					buffer[0] = -1;
+			}
+			return buffer[index++];
+		}
+	}
+}
