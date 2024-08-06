e7a168450c1161175844aa54fc788c9fb672b436
taehoon <xogns2134@gmail.com>
2024-08-06 19:44:56 +0900

    [Bronze IV] Title: 상근날드, Time: 124 ms, Memory: 14068 KB -BaekjoonHub

---
+++ 백준/Bronze/5543. 상근날드/상근날드.java
@@ -0,0 +1,55 @@
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Reader reader = new Reader();
+		int sum = 0;
+		int min = Integer.MAX_VALUE;
+		for (int i = 0; i < 3; i++) {
+			min = Math.min(min, reader.nextInt());
+		}
+		sum += min;
+		min = Integer.MAX_VALUE;
+
+		for (int i = 0; i < 2; i++) {
+			min = Math.min(min, reader.nextInt());
+		}
+
+		sum += min;
+
+		System.out.println(sum - 50);
+
+	}
+
+	static class Reader {
+		final int SIZE = 1 << 15;
+		byte[] buffer = new byte[SIZE];
+		int index, size;
+
+		int nextInt() throws IOException {
+			int n = 0;
+			byte c;
+			while ((c = read()) <= 32)
+				;
+			boolean neg = c == '-';
+			if (neg)
+				c = read();
+			do
+				n = (n << 3) + (n << 1) + (c & 15); while (isNumber(c = read()));
+			return neg ? -n : n;
+		}
+
+		boolean isNumber(byte c) {
+			return 47 < c && c < 58;
+		}
+
+		byte read() throws IOException {
+			if (index == size) {
+				size = System.in.read(buffer, index = 0, SIZE);
+				if (size < 0)
+					buffer[0] = -1;
+			}
+			return buffer[index++];
+		}
+	}
+}
