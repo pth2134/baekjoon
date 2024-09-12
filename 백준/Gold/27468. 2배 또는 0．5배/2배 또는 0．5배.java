90b0036ccabe14dc6bc30e1199bb3fa4f1935dbf
taehoon <xogns2134@gmail.com>
2024-09-12 15:00:22 +0900

    [Gold V] Title: 2배 또는 0.5배, Time: 384 ms, Memory: 68484 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Gold/27468. 2배 또는 0．5배/2배 또는 0．5배.java
@@ -0,0 +1,60 @@
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Reader reader = new Reader();
+		StringBuilder sb = new StringBuilder();
+		int n = reader.nextInt();
+
+		int[] five = {1, 2, 4, 3};
+		int[] four = {2, 1, 3, 4};
+
+		int[] arr;
+
+		if (n % 4 == 1)
+			arr = five;
+		else
+			arr = four;
+
+		for (int i = 0; i < n; i++) {
+			sb.append(arr[i % 4]).append(' ');
+			arr[i % 4] += 4;
+		}
+		System.out.println("YES");
+		System.out.println(sb);
+
+	}
+
+	static class Reader {
+		final int SIZE = 1 << 15;
+		byte[] buffer = new byte[SIZE];
+		int index, size;
+
+		int nextInt() throws IOException {
+			int n = 0;
+			byte c;
+			while ((c = read()) <= 32)
+				;
+			boolean neg = c == '-';
+			if (neg)
+				c = read();
+			do
+				n = (n << 3) + (n << 1) + (c & 15); while (isNumber(c = read()));
+			return neg ? -n : n;
+		}
+
+		boolean isNumber(byte c) {
+			return 47 < c && c < 58;
+		}
+
+		byte read() throws IOException {
+			if (index == size) {
+				size = System.in.read(buffer, index = 0, SIZE);
+				if (size < 0)
+					buffer[0] = -1;
+			}
+			return buffer[index++];
+		}
+	}
+}
