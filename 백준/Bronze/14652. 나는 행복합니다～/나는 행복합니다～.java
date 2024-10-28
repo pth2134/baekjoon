ad307c89660be37e56ffe95fc48fdb8a634f974a
taehoon <xogns2134@gmail.com>
2024-10-28 14:01:01 +0900

    [Bronze IV] Title: 나는 행복합니다~, Time: 128 ms, Memory: 16008 KB -BaekjoonHub

---
+++ 백준/Bronze/14652. 나는 행복합니다～/나는 행복합니다～.java
@@ -0,0 +1,47 @@
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Reader reader = new Reader();
+		int n = reader.nextInt();
+		int m = reader.nextInt();
+		int k = reader.nextInt();
+
+		int y = k / m;
+		int x = k % m;
+		System.out.println(y + " " + x);
+	}
+
+	static class Reader {
+		final int SIZE = 1 << 15;
+		byte[] buffer = new byte[SIZE];
+		int index, size;
+
+		int nextInt() throws IOException {
+			int n = 0;
+			byte c;
+			while ((c = read()) <= 32)
+				;
+			boolean neg = c == '-';
+			if (neg)
+				c = read();
+			do
+				n = (n << 3) + (n << 1) + (c & 15); while (isNumber(c = read()));
+			return neg ? -n : n;
+		}
+
+		boolean isNumber(byte c) {
+			return 47 < c && c < 58;
+		}
+
+		byte read() throws IOException {
+			if (index == size) {
+				size = System.in.read(buffer, index = 0, SIZE);
+				if (size < 0)
+					buffer[0] = -1;
+			}
+			return buffer[index++];
+		}
+	}
+}
