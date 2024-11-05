fda380a74bf92e5486783e418f75c2c8d083c73b
taehoon <xogns2134@gmail.com>
2024-11-05 20:12:10 +0900

    [Bronze IV] Title: 입실 관리, Time: 104 ms, Memory: 14104 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5524. 입실 관리/입실 관리.java
@@ -0,0 +1,62 @@
+import java.io.IOException;
+
+public class Main {
+
+	private void solution() throws Exception {
+		Reader reader = new Reader();
+		int n = reader.nextInt();
+		StringBuilder sb = new StringBuilder();
+		while (n-- > 0) {
+			String cur = reader.nextString();
+			sb.append(cur.toLowerCase()).append('\n');
+		}
+		System.out.print(sb);
+	}
+
+	public static void main(String[] args) throws Exception {
+		new Main().solution();
+	}
+
+	static class Reader {
+		final int SIZE = 1 << 15;
+		byte[] buffer = new byte[SIZE];
+		int index, size;
+
+		int nextInt() throws IOException {
+			int n = 0;
+			byte c;
+			while ((c = read()) <= 32)
+				;
+			boolean neg = c == '-';
+			if (neg)
+				c = read();
+			do
+				n = (n << 3) + (n << 1) + (c & 15); while (isNumber(c = read()));
+			return neg ? -n : n;
+		}
+
+		boolean isNumber(byte c) {
+			return 47 < c && c < 58;
+		}
+
+		byte read() throws IOException {
+			if (index == size) {
+				size = System.in.read(buffer, index = 0, SIZE);
+				if (size < 0)
+					buffer[0] = -1;
+			}
+			return buffer[index++];
+		}
+
+		String nextString() throws IOException {
+			StringBuilder sb = new StringBuilder();
+			byte c;
+			while ((c = read()) <= 32)
+				;
+			do {
+				sb.append((char)c);
+			} while ((c = read()) > 32);
+			return sb.toString();
+		}
+	}
+}
