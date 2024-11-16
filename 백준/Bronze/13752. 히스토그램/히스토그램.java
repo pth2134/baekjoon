e8516a0270a7e94be2f27e90650e005bb1e5257d
taehoon <xogns2134@gmail.com>
2024-11-16 16:31:48 +0900

    [Bronze IV] Title: 히스토그램, Time: 104 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.java
@@ -0,0 +1,56 @@
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		Reader reader = new Reader();
+		StringBuilder sb = new StringBuilder();
+		int n = reader.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			int input = reader.nextInt();
+			sb.append("=".repeat(Math.max(0, input)));
+			sb.append('\n');
+		}
+		System.out.println(sb);
+	}
+
+	static class Reader {
+		final int SIZE = 1 << 15;
+		byte[] buffer = new byte[SIZE];
+		int index, size;
+
+		int nextInt() throws IOException {
+			int n = 0;
+			byte c;
+			while ((c = read()) <= 32)
+				;
+			boolean neg = c == '-';
+			if (neg)
+				c = read();
+			do
+				n = (n << 3) + (n << 1) + (c & 15); while (isNumber(c = read()));
+			return neg ? -n : n;
+		}
+
+		boolean isNumber(byte c) {
+			return 47 < c && c < 58;
+		}
+
+		byte read() throws IOException {
+			if (index == size) {
+				size = System.in.read(buffer, index = 0, SIZE);
+				if (size < 0)
+					buffer[0] = -1;
+			}
+			return buffer[index++];
+		}
+
+		char nextChar() throws IOException {
+			byte c;
+			while ((c = read()) <= 32)
+				;
+			return (char)c;
+		}
+	}
+}
